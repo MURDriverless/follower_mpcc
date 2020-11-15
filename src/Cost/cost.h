e705bda7c352b78ef359f13bc79588ec1e93a80d
Dennis Wirya <dwirya@student.unimelb.edu.au>
2020-11-16 02:55:33 +1100

    Done with contouring cost (also includes max. progress term)

--- src/Cost/cost.h
+++ src/Cost/cost.h
@@ -5,10 +5,14 @@
 #ifndef FOLLOWER_MPCC_COST_H
 #define FOLLOWER_MPCC_COST_H
 
+#include <cmath>
+#include <Eigen/Dense>
 #include "../config.h"
 #include "../types.h"
 #include "../Splines/cubic_spline2d.h"
 #include "../Params/params.h"
+
+using Eigen::Matrix;
 
 
 // Return type for getCost() function below
@@ -31,10 +35,29 @@ struct CostTerm {
 };
 
 
+// Position and partial derivatives of x, y and yaw with respect to theta (spline parameter)
+struct RefPoint {
+    const double x;
+    const double y;
+    const double dx;
+    const double dy;
+    const double yaw;
+    const double dyaw;
+};
+
+// Contains the exact non-linear error and Jacobian of the error at the predicted "xk"
+struct ContouringError {
+    const Matrix<double, 2, 1> error;
+    const Matrix<double, 2, NX> d_error;
+};
+
+
 class Cost {
 public:
-    CostMatrix getCost();
+    CostMatrix getCost(const CubicSpline2D &path, const State &xk);
 private:
+    static RefPoint getRefPoint(const CubicSpline2D &path, const State &xk);
+    static ContouringError getContouringError(const CubicSpline2D &path, const State &xk);
     CostTerm<Q_MPC, q_MPC> getContouringCost(const CubicSpline2D &path, const State &xk) const;
     CostTerm<Q_MPC, q_MPC> getRawInputCost() const;
     CostTerm<R_MPC, r_MPC> getInputChangeCost() const;

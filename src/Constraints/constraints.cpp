e911c0c8566442fc6f6e35f00cb76f43aa81bc47
Dennis Wirya <dwirya@student.unimelb.edu.au>
2020-11-25 02:11:57 +1100

    Fix compilation issue due to illegal constructor for Matrix2d

--- src/Constraints/constraints.cpp
+++ src/Constraints/constraints.cpp
@@ -25,11 +25,13 @@ Constraint1D Constraints::getTrackConstraint(const Track &track, const State &xk
     const Vector2d diff_vec = outer_pos - centre_pos;
     const double diff_vec_norm_gain = 1 / (pow(diff_vec(0), 2) + pow(diff_vec(1), 2));
 
+    // Container to fix the inability to use Matrix2d constructor like: Matrix2d { el(0), el(1), el(2), el(3) }
+    Matrix2d T_matrix;
+    T_matrix << diff_vec(0), diff_vec(1),
+               -diff_vec(1), diff_vec(0);
+
     // G
-    const Matrix2d G = diff_vec_norm_gain * Matrix<double, 2, 2> {
-        diff_vec(0), diff_vec(1),
-        -diff_vec(1), diff_vec(0)
-    };
+    const Matrix<double, 2, 2> G = diff_vec_norm_gain * T_matrix;
 
     // Construct Jacobian first
     Matrix<double, 2, NX> J_p = Matrix<double, 2, NX>::Zero();
@@ -41,16 +43,14 @@ Constraint1D Constraints::getTrackConstraint(const Track &track, const State &xk
     // dG_dtheta
     const Vector2d J_centre_pos = track.centre.getDerivative(s);
     const Vector2d J_diff_vec = track.outer.getDerivative(s) - J_centre_pos;
-    const Matrix2d dG_dtheta_1 = diff_vec_norm_gain * Matrix2d {
-        J_diff_vec(0), J_diff_vec(1),
-        -J_diff_vec(1), J_diff_vec(0)
-    };
+    // Container to fix the inability to use Matrix2d constructor like: Matrix2d { el(0), el(1), el(2), el(3) }
+    Matrix2d J_T_matrix;
+    J_T_matrix << J_diff_vec(0), J_diff_vec(1),
+                 -J_diff_vec(1), J_diff_vec(0);
+    const Matrix2d dG_dtheta_1 = diff_vec_norm_gain * J_T_matrix;
     const Matrix2d dG_dtheta_2 = pow(diff_vec_norm_gain, 2) *
                                 (2*diff_vec(0)*J_diff_vec(0) + 2*diff_vec(1)*J_diff_vec(1)) *
-                                Matrix2d {
-                                    diff_vec(0), diff_vec(1),
-                                    -diff_vec(1), diff_vec(0)
-                                };
+                                T_matrix;
     const Matrix2d dG_dtheta = dG_dtheta_1 - dG_dtheta_2;
 
     // dp_dtheta
@@ -72,7 +72,7 @@ Constraint1D Constraints::getTrackConstraint(const Track &track, const State &xk
     const double lower_bound_x = lower_bound(0);
     const double upper_bound_x = upper_bound(1);
 
-    return { J_p, lower_bound_x, upper_bound_x };
+    return { J_p_x, lower_bound_x, upper_bound_x };
 }
 
 Constraint1D Constraints::getRearTireConstraint(const State &xk) const {
